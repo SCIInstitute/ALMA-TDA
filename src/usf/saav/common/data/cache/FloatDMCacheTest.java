40c9e180a8497208fabe1b76b1d518dee6b5a8bb
Dan White <dwhite@sci.utah.edu>
2016-11-02 13:18:42 -0600

    Test stub

---
+++ src/usf/saav/common/data/cache/FloatDMCacheTest.java
@@ -0,0 +1,104 @@
+package usf.saav.common.data.cache;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FloatDMCacheTest {
+
+	@Test
+	public void testCreatePage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFloatDMCacheStringBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFloatDMCacheStringBooleanBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFloatDMCacheStringIntIntBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFloatDMCacheStringIntIntBooleanBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testWriteBackAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPrintPageInfo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCurrentPageCount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetPage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDirectMappedCache() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testBaseCache() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testClose() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPrintCacheInfo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCurrentDataSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetDataSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetFileSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetPageCount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
