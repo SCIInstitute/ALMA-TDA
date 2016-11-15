af49b3c9e7c58c195479f1269e406b673662ce01
Dan White <dwhite@sci.utah.edu>
2016-11-15 12:49:58 -0700

    Easy tests

--- src/usf/saav/common/data/cache/FloatDMCacheTest.java
+++ src/usf/saav/common/data/cache/FloatDMCacheTest.java
@@ -2,33 +2,38 @@ package usf.saav.common.data.cache;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FloatDMCacheTest {
 
-	@Test
-	public void testCreatePage() {
-		fail("Not yet implemented"); // TODO
+	private static final int	 page_size_bytes = 8096;
+	private static final int	 page_size_elems = 2048;
+	private static final int	 page_count      = 2048;
+	private static final boolean use_zorder 	 = true; 
+	
+	private static File cache_file;
+	private static FloatDMCache cache;
+	
+	@BeforeClass
+	public static void setupBeforeClass() throws IOException
+	{
+		cache_file = new File("C:\\alma_data\\fits1\\Continuum_33GHz.fits.cache");
+		cache = new FloatDMCache( cache_file.getAbsolutePath(), page_size_bytes, page_count, false, false );
 	}
-
+	
 	@Test
-	public void testFloatDMCacheStringBoolean() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testFloatDMCacheStringBooleanBoolean() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testFloatDMCacheStringIntIntBoolean() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testFloatDMCacheStringIntIntBooleanBoolean() {
-		fail("Not yet implemented"); // TODO
+	public void testFloatDMCacheStringIntIntBooleanBoolean() throws IOException {
+		assertTrue(cache != null);
+		
+		assertEquals(16580608, cache.GetCurrentDataSize());
+		assertEquals(16580608, cache.GetDataSize());
+		assertEquals(275264, cache.GetFileSize());
+		assertEquals(2048, cache.GetPageCount());
+		assertEquals(2048, cache.GetCurrentPageCount());
 	}
 
 	@Test
@@ -47,28 +52,8 @@ public class FloatDMCacheTest {
 	}
 
 	@Test
-	public void testPrintPageInfo() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
 	public void testGetCurrentPageCount() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testGetPage() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testDirectMappedCache() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testBaseCache() {
-		fail("Not yet implemented"); // TODO
+		assertEquals(2048, cache.GetCurrentPageCount());
 	}
 
 	@Test
@@ -77,28 +62,23 @@ public class FloatDMCacheTest {
 	}
 
 	@Test
-	public void testPrintCacheInfo() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
 	public void testGetCurrentDataSize() {
-		fail("Not yet implemented"); // TODO
+		assertEquals(16580608, cache.GetCurrentDataSize());
 	}
 
 	@Test
 	public void testGetDataSize() {
-		fail("Not yet implemented"); // TODO
+		assertEquals(16580608, cache.GetDataSize());
 	}
 
 	@Test
-	public void testGetFileSize() {
-		fail("Not yet implemented"); // TODO
+	public void testGetFileSize() throws IOException {
+		assertEquals(275264, cache.GetFileSize());
 	}
 
 	@Test
 	public void testGetPageCount() {
-		fail("Not yet implemented"); // TODO
+		assertEquals(2048, cache.GetPageCount());
 	}
 
 }
