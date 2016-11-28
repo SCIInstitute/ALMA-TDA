42ee43bdb54deb6a81c8b29565babd5a6db78cb2
Dan White <dwhite@sci.utah.edu>
2016-11-28 12:03:24 -0700

    CachedFitsReader test stub

---
+++ src/usf/saav/alma/data/fits/CachedFitsReaderTest.java
@@ -0,0 +1,64 @@
+package usf.saav.alma.data.fits;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CachedFitsReaderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testCachedFitsReader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAxesSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetElement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRowIntRange1DIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColumnIntIntRange1DIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLineIntIntIntRange1DInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSliceIntRange1DIntRange1DIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVolumeIntRange1DIntRange1DIntRange1DInt() {
+		fail("Not yet implemented");
+	}
+
+}
